c22c1d1cbbff9a6db2b88ab229814edacadde631
Demian-Sol <demian_messages@mail.ru>
2019-04-18 00:15:07 +0700

    project initial commit, add fetch data logic

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const CONTACTS_LS_LABEL = 'CONTACTS_LS_LABEL';
+export const DATA_SOURCE_URL = 'http://demo.sibers.com/users';
---
+++ src/store/types.js
@@ -0,0 +1,2 @@
+export const SET_CONTACTS_DATA = 'SET_CONTACTS_DATA';
+export const SET_ERROR_INFO = 'SET_ERROR_INFO';
