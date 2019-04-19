4fc2638f9a02ced8dcc09bb27039c6297d54f626
Demian-Sol <demian_messages@mail.ru>
2019-04-19 11:27:42 +0700

    add alphabetical sorting

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,4 +1,13 @@
-export const selectListDataAll = state => state.contacts.map(contact => {
-  const { name, username, favorite, id } = contact;
-  return { name, username, favorite, id };
+const sortAlphabeticallyBy = (array, prop) => array.sort((a, b) => {
+  if (a[prop] < b[prop]) return -1;
+  if (a[prop] > b[prop]) return 1;
+  return 0;
 });
+
+export const selectListDataAll = state => {
+  const unsortedArray = state.contacts.map(contact => {
+    const { name, username, favorite, id } = contact;
+    return { name, username, favorite, id };
+  });
+  return sortAlphabeticallyBy(unsortedArray, 'name');
+};
