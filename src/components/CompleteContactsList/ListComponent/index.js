6cdbc60dd67d05c9b6988ef97fd24f4f9fc19154
Demian-Sol <demian_messages@mail.ru>
2019-04-19 12:39:22 +0700

    add contacts grouping

--- src/components/CompleteContactsList/ListComponent/index.js
+++ src/components/CompleteContactsList/ListComponent/index.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import ListElement from './ListElement';
+import GroupingElement from './GroupingElement';
 
 const propTypes = {
   listData: PropTypes.array.isRequired,
 };
 
 const ListComponent = ({ listData }) => (
-  listData.map(listElement => (
-    <ListElement
-      key={listElement.id}
-      listElement={listElement}
-    />
-  ))
+  listData.map((listElement, index, arr) => {
+    if (index === 0 || listElement.name.charAt(0) !== arr[index - 1].name.charAt(0)) {
+      return (
+        <Fragment key={listElement.id}>
+          <GroupingElement
+            letter={listElement.name.charAt(0)}
+          />
+          <ListElement
+            listElement={listElement}
+          />
+        </Fragment>
+      );
+    }
+    return (
+      <ListElement
+        key={listElement.id}
+        listElement={listElement}
+      />
+    );
+  })
 );
 
 ListComponent.propTypes = propTypes;
